08ca18560ca61aad5242476461483cdee2b31cb3
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-05 20:53:03 +0600

    styles done in every book

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -6,7 +6,7 @@ const Books = () => {
     const loadData = useLoaderData();
     // console.log(loadData.books)
     return (
-        <div className='px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+        <div className='px-5 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 mt-7'>
             {
                 loadData.books.map(book => <ShowBooks key={book.isbn13} book={book}></ShowBooks>)
             }
