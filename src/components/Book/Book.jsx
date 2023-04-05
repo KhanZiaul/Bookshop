fd549974cc254c2d5c9bdc1e16ab96d8acceabea
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-05 21:12:37 +0600

    image added click on single book

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const Book = () => {
+    const bookDetails = useLoaderData()
+    console.log(bookDetails)
+    const {image , authors , publisher , price , rating , year , title , desc} = bookDetails;
     return (
-        <div>
-            <h2>Book</h2>
+        <div className='border-2 w-[60%] mx-auto flex mt-7'>
+            <div>
+                <img src={image} alt="" />
+            </div>
+            <div>
+
+            </div>
+
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
       {
         path: 'book/:id',
         element: <Book></Book>,
-        loader:(params) => fetch(`https://api.itbook.store/1.0/books/${params.id}`)
+        loader:({params}) => fetch(`https://api.itbook.store/1.0/books/${params.id}`)
       },
       {
         path: 'about',
