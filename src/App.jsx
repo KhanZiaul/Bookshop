3d9e3d8a1983eded29dc4a944d7a7d32994f14ae
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-05 17:44:28 +0600

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Header/Nav';
 import { Outlet } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
 
 const App = () => {
   return (
-    <div className='w-[95%] mx-auto mt-2'>
+    <div className='w-[95%] mx-auto mt-2 '>
      <Nav></Nav>
      <Outlet></Outlet>
+     <Footer></Footer>
     </div>
   );
 };
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer>
+            <h2 className='text-center my-5'>© 2023 nextPage Inc. All rights reserved.</h2>
+        </footer>
+    );
+};
+
+export default Footer;
