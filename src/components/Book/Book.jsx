0c07e6eed5c45dc3b4af7450d4bdc7946b7d1a35
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-05 21:34:10 +0600

    authors,publisher , desc , year added after click on single book

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -4,16 +4,21 @@ import { useLoaderData } from 'react-router-dom';
 const Book = () => {
     const bookDetails = useLoaderData()
     console.log(bookDetails)
-    const {image , authors , publisher , price , rating , year , title , desc} = bookDetails;
+    const { image, authors, publisher, price, rating, year, title, desc } = bookDetails;
     return (
-        <div className='border-2 w-[60%] mx-auto flex mt-7'>
-            <div>
-                <img src={image} alt="" />
+        <div className='border-2 mx-auto flex mt-7 px-6'>
+            <div className='w-[40%]'>
+                <img src={image} alt="no image" />
             </div>
-            <div>
-
+            <div className=''>
+                <p className='text-4xl font-semibold mb-3'>{title}</p>
+                <p>Authors : {authors}</p>
+                <p>Publisher : {publisher}</p>
+                <p>Rating : {rating}</p>
+                <p className='mb-3'>Year : {year}</p>
+                <p className='mb-5'>{desc.slice(0, 150) + ".........."}</p>
+ 
             </div>
-
         </div>
     );
 };
