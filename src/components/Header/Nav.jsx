1926a61b664ad47bbeb0d0edf4554e83f16c9981
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-05 23:55:46 +0600

    navigation updated for small device

--- src/components/Header/Nav.jsx
+++ src/components/Header/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import { BookOpenIcon } from '@heroicons/react/24/solid'
+import { XMarkIcon , Bars4Icon } from '@heroicons/react/24/solid'
 import './Nav.css'
 
 const Nav = () => {
@@ -13,7 +14,13 @@ const Nav = () => {
                 <h2 className='text-2xl font-semibold'>BookShop</h2>
             </Link>
 
-            <div className='inline-flex gap-7 font-semibold'>
+            <div>
+            <div className='block lg:hidden relative'>
+                {
+                    navigation ? <Bars4Icon onClick={()=> setNavigation(!navigation)} className='w-8 h-8 cursor-pointer'></Bars4Icon> : <XMarkIcon onClick={()=> setNavigation(!navigation)} className='w-8 h-8 cursor-pointer'></XMarkIcon>
+                }
+            </div>
+            <div className={`absolute ${navigation ? 'top-20' : '-top-40'} duration-200 md:static inline-flex flex-col md:flex-row gap-7 font-semibold`}>
                 <NavLink
                     to="/"
                     className={({ isActive }) => isActive ? "active" : ""}>
@@ -29,6 +36,7 @@ const Nav = () => {
                     className={({ isActive }) => isActive ? "active" : ""}>
                     About us
                 </NavLink>
+            </div>
             </div>
         </div>
     );
