5ee47e6eb43fa1040d5b0bf9b4f6416dff6d8cee
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-04-05 22:58:33 +0600

    about second part done

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
       <h2 className='text-center text-4xl font-bold mb-4'>before he dies</h2>
       <p  className='text-center'>Books are a uniquely portable magic. Books serve to show a man that those original <br /> thoughts of his aren't very new after all. The man who does not read good books is <br /> no better than the man who can't.</p>
 
-      <div className='grid grid-cols-2'>
+      <div className='md:grid grid-cols-2 gap-10 mt-10'>
 
         <div>
         <h2 className='text-xl font-bold mb-4'>Books are a uniquely portable magic.</h2>
@@ -21,8 +21,20 @@ const About = () => {
         <h2 className='text-xl font-bold mb-4'>Is the Space Pope reptilian!?</h2>
         <p className='mb-5'>A flower in my garden, a mystery in my panties. Heart attack never stopped old Big Bear. I didn't even know we were calling him Big Bear.</p>
         </div>
+        
 
+        <div>
+        <h2 className='text-xl font-bold mb-4'>How much money you got on you?</h2>
+        <p className='mb-5'>The first mate and his Skipper too will do their very best to make the others comfortable in their tropic island nest.</p>
+        <p className='mb-5'>Michael Knight a young loner on a crusade to champion the cause of the innocent. The helpless. The powerless in a world of criminals who operate above the law. Here he comes Here comes Speed Racer. He's a demon on wheels.</p>
 
+        <h2 className='text-xl font-bold mb-4'>Galaxies Orion's sword globular star cluster?</h2>
+        <p className='mb-5'>A business big enough that it could be listed on the NASDAQ goes belly up. Disappears!</p>
+        <p className='mb-5'>It ceases to exist without me. No, you clearly don't know who you're talking to, so let me clue you in.</p>
+
+        <h2 className='text-xl font-bold mb-4'>When has justice ever been as simple as a rule book?</h2>
+        <p className='mb-5'>This is not about revenge. This is about justice. A lot of things can change in twelve years, Admiral. Well, that's certainly good to know. About four years. I got tired of hearing how young I looked.</p>
+        </div>
 
       </div>
         
